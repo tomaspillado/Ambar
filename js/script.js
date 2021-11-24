40092ad7cdaddab04be574fd6d516adfdc541d63
Tomas Pillado <tomascarck@gmail.com>
2021-11-24 13:13:35 -0300

    finalizando

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@ import menuHamburger from "../js/hamburger.js";
 import slider from "../js/slider.js";
 
 addEventListener("DOMContentLoaded",()=>{
-    console.log("hola");
     menuHamburger(".menu-btn",".lista",".item");
     scrollButton(".scroll-btn");
     slider("#slider",".slider-brumas","#btn-right","#btn-left");
 })
  
-alert("hola");
